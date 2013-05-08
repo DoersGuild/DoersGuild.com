2c9ca6f8617f4e670ba41db290ae29448b876b4c
Sathvik P <psathvik@gmail.com>
2013-05-08 20:22:39 +0530

    HTML Template

--- init/init.go
+++ init/init.go
@@ -2,13 +2,28 @@ package init
 
 import (
     "fmt"
+    "html/template"
     "net/http"
 )
 
 func init() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/temp", temp)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+    fmt.Fprint(w, "Hello, world!!!!")
+}
+
+func temp(w http.ResponseWriter, r *http.Request) {
+
+  var listTmpl = template.Must(template.ParseFiles("tmpl/welcome.html"))
+
+  tc := make(map[string]interface{})
+  tc["Store"] = "HEY"
+  
+  if err := listTmpl.Execute(w, tc); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+  
 }
