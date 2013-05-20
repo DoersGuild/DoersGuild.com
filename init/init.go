76d6215be2b6c27c1b416fa1f700cd0c68102920
Sathvik P <psathvik@gmail.com>
2013-05-20 20:44:57 +0530

    Portfolio Category and Details Pages

--- init/init.go
+++ init/init.go
@@ -5,6 +5,7 @@ import (
     "html/template"
     "net/http"
     "github.com/gorilla/mux"
+    "appengine"
 )
 
 // Acommon variable with data for templates
@@ -19,36 +20,60 @@ func init() {
 	router:=mux.NewRouter()
 	router.HandleFunc("/", indexHandler)
 	router.HandleFunc("/portfolio", portfolioHandler)
-	router.HandleFunc("/portfolio/{category}", portfolioHandler)
-	router.HandleFunc("/portfolio/{category}/{project}", portfolioHandler)
+	router.HandleFunc("/portfolio/{category}", portfolioCategoryHandler)
+	router.HandleFunc("/portfolio/{category}/{project}", portfolioDetailsHandler)
     router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	http.Handle("/", router)
 }
 
-func executeSimpleTemplate(w http.ResponseWriter, tmplFile string) {
+func executeSimpleTemplate(w http.ResponseWriter, r *http.Request, tmplFile string) {
 	// Load and execute the given template
+	
+	// Log stats
+	c := appengine.NewContext(r)
+	c.Infof("Requested URL: %v", r.URL)
+	c.Infof("Loading Template: %v", tmplFile)
     
-  var listTmpl = template.Must(template.ParseFiles("tmpl/base.html", tmplFile))
-  
-  if err := listTmpl.Execute(w, config); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
+	var listTmpl = template.Must(template.ParseFiles("tmpl/base.html", tmplFile))
+	  
+	if err := listTmpl.Execute(w, config); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
   
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	// The 404 page
-	executeSimpleTemplate(w, "tmpl/content/404.html")
+	executeSimpleTemplate(w, r, "tmpl/content/404.html")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// The home page
-	executeSimpleTemplate(w, "tmpl/content/index.html")
+	executeSimpleTemplate(w, r, "tmpl/content/index.html")
 }
 
 func portfolioHandler(w http.ResponseWriter, r *http.Request) {
 	// The portfolio page
 	config["portfolioItems"] = portfolioItems
 	config["portfolioCategories"] = portfolioCategories
-	executeSimpleTemplate(w, "tmpl/content/portfolio.html")
+	executeSimpleTemplate(w, r, "tmpl/content/portfolio.html")
+}
+
+func portfolioCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	// The portfolio category listing page
+	vars := mux.Vars(r)
+	category:=vars["category"]
+	config["portfolioItems"] = portfolioItems[category]
+	config["portfolioCategory"] = portfolioCategories[category]
+	executeSimpleTemplate(w, r, "tmpl/content/portfolio_category.html")
+}
+
+func portfolioDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	// The portfolio details page
+	vars := mux.Vars(r)
+	category:=vars["category"]
+	project:=vars["project"]
+	config["portfolioItem"] = portfolioItems[category].(map[string]PortfolioItem)[project]
+	config["portfolioCategory"] = portfolioCategories[category]
+	executeSimpleTemplate(w, r, "tmpl/content/portfolio_details.html")
 }
--- init/portfolioData.go
+++ init/portfolioData.go
@@ -9,6 +9,7 @@ type PortfolioItem struct {
     Title string
     ShortDesc string
     Date string
+    Links map[string]string
     Icon string
     Screenshots []string
     Tags []string
@@ -20,32 +21,35 @@ var portfolioCategories = map[string]Category {
 }
 
 var portfolioItems = map[string]interface{} {
-	"MobileApps" : []PortfolioItem {
-		{
+	"MobileApps" : map[string]PortfolioItem {
+		"GeneralKnowitallKnowledge" : {
 			"GeneralKnowitallKnowledge",
 			"General Knowitall Knowledge",
 			"General Knowitall Knowledge is a fun android app for all the quizzing enthusiasts out there. It comes loaded with fun and interesting facts about various things from around the world.",
 			"November, 2012",
+			map[string]string{"PlayStore" : "http:play.google.com", "Blog":"http://blog.doersguild.com"},
 			"/img/logos/mobile/gkk.png",
 			[]string{},
 			[]string{"PhoneGap","Android", "Twitter Bootstrap"},
 		},
-		{
+		"iEntertain" : {
 			"iEntertain",
 			"i-Entertain",
 			"A Mobile Gallery Application that shows it’s users, incredibly funny images from around the world, leaving them fully entertained. It is designed to run as both a website and a native android application and was built with the standard web technologies and packaged with PhoneGap. This application’s UI is fully responsive, so it works extremely well on all modern devices and their various screen sizes.",
 			"September, 2012",
+			map[string]string{"PlayStore" : "http:play.google.com"},
 			"/img/logos/mobile/i-entertain.jpg",
 			[]string{},
 			[]string{"PhoneGap","Android", "jQuery Mobile"},
 		},
 	},
-	"WebApps" : []PortfolioItem {
-		{
+	"WebApps" : map[string]PortfolioItem {
+		"IXXOCart" : {
 			"IXXOCart",
 			"IXXO Templating framework",
 			"We helped IXXO-cart upgrade their templating framework with site-modules, twitter-bootstrap, JSON-web-services, etc and created several new templates for their various offerings including Multi-Vendor cart, Mobile templates, etc.",
 			"May, 2013",
+			map[string]string{"MVDemo" : "http://ixxocart.com", "Blog":"http://blog.doersguild.com"},
 			"/img/logos/mobile/ixxo.png",
 			[]string{},
 			[]string{"PHP", "Smarty", "Twitter Bootstrap", "IXXOCart"},
