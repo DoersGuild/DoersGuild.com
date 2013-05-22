c3317ab9500a197ef903375cb3c0fa4269fa6ee2
Sathvik P <psathvik@gmail.com>
2013-05-22 19:15:44 +0530

    Cheriyal And Pembarthy

--- init/portfolioData.go
+++ init/portfolioData.go
@@ -8,6 +8,7 @@ var portfolioClients = map[string]Person {
 	"DoersGuild" : {"DoersGuild", "Doers' Guild", "Doers' Guild", "http://doersguild.com", ""},
 	"WissamSaleh" : {"WissamSaleh", "Wissam Saleh", "", "http://ientertain.mobi", ""},
 	"IXXOCart" : {"IXXOCart", "IXXO Cart", "IXXO Cart", "http://ixxocart.com", "/img/projects/IXXOCart/logo.png"},
+	"Tharuni" : {"Tharuni","Tharuni","Tharuni","http://tharuni.org","/img/projects/Tharuni/logo.png"},
 	"GKK_Rahul" : {"GKK_Rahul", "Rahul", "", "https://play.google.com/store/apps/details?id=com.doersguild.generalknowledge&reviewId=bGc6QU9xcFRPSDZUNEdCRlVLWjBGaF9WbGtaWDNVdFZFcjN2ZkZacXk5OVFpWEFwbkpwVk1HNmRKSlJlTTE5bEEwcS1rVG1sLThRRUlOYmRURU1Ca2F6NGFz", ""},
 }
 
@@ -37,7 +38,7 @@ var portfolioItems = map[string]PortfolioItem {
 		"General Knowitall Knowledge is a fun android app for all the quizzing enthusiasts out there. It comes loaded with fun and interesting facts about various things from around the world.",
 		"November, 2012",
 		"MobileApps",
-		map[string]string{"PlayStore" : "http://play.google.com", "Blog":"http://blog.doersguild.com"},
+		map[string]string{"Play Store" : "https://play.google.com/store/apps/details?id=com.doersguild.generalknowledge", "Ad-Free Edition":"https://play.google.com/store/apps/details?id=com.doersguild.gkkplus"},
 		"/img/projects/GeneralKnowitallKnowledge/logo.png",
 		[]string{"/img/projects/GeneralKnowitallKnowledge/mobile1.png", "/img/projects/GeneralKnowitallKnowledge/tablet1.png"},
 		[]string{"PhoneGap","Android", "Twitter Bootstrap"},
@@ -52,14 +53,12 @@ var portfolioItems = map[string]PortfolioItem {
 		"A Mobile Gallery Application that shows it’s users, incredibly funny images from around the world, leaving them fully entertained. It is designed to run as both a website and a native android application and was built with the standard web technologies and packaged with PhoneGap. This application’s UI is fully responsive, so it works extremely well on all modern devices and their various screen sizes.",
 		"September, 2012",
 		"MobileApps",
-		map[string]string{"PlayStore" : "http://play.google.com"},
+		map[string]string{"Play Store" : "https://play.google.com/store/apps/details?id=com.WissamSaleh.iEntertain"},
 		"/img/projects/iEntertain/logo.jpg",
 		[]string{},
 		[]string{"PhoneGap","Android", "jQuery Mobile"},
 		portfolioClients["WissamSaleh"],
-		[]Feedback{
-			{"iEntertain", "WissamSaleh", "Very Responsive, really enjoyed working with you guys",},
-		},
+		[]Feedback{},
 	},
 	"IXXOCart" : {
 		"IXXOCart",
@@ -67,14 +66,25 @@ var portfolioItems = map[string]PortfolioItem {
 		"We helped IXXO-cart upgrade their templating framework with site-modules, twitter-bootstrap, JSON-web-services, etc and created several new templates for their various offerings including Multi-Vendor cart, Mobile templates, etc.",
 		"May, 2013",
 		"WebApps",
-		map[string]string{"MVDemo" : "http://ixxocart.com", "Blog":"http://blog.doersguild.com"},
+		map[string]string{"Multi-Vendor Demo" : "http://demo-multi-vendor-sa.ixxocart.com/index.php"},
 		"/img/projects/IXXOCart/logo.png",
 		[]string{},
 		[]string{"PHP", "Smarty", "Twitter Bootstrap", "IXXOCart"},
 		portfolioClients["IXXOCart"],
-		[]Feedback{
-			{"IXXOCart", "IXXOCart", "The best development agency I've worked with in the last 20 years.",},
-		},
+		[]Feedback{},
+	},
+	"CheriyalAndPembarthy": {
+		"CheriyalAndPembarthy",
+		"Cheriyal & Pembarthy Websites",
+		"We built responsive e-commerce websites for the artisans of Cheriyal and Pembarthy villages to better educate the world about their artwork and sell it online.",
+		"January, 2013",
+		"Websites",
+		map[string]string{"Cheriyal Paintings" : "http://cheriyalpainitngs.com", "Pembarthy Brassware":"http://pembarthybrassware.com"},
+		"http://cheriyalpaintings.com/assets/DSC01542.jpg",
+		[]string{"/img/projects/CheriyalAndPembarthy/desktop2.png","/img/projects/CheriyalAndPembarthy/desktop1.png","/img/projects/CheriyalAndPembarthy/tablet1.png","/img/projects/CheriyalAndPembarthy/tablet2.png","/img/projects/CheriyalAndPembarthy/mobile1.png","/img/projects/CheriyalAndPembarthy/mobile2.png"},
+		[]string{"WordPress", "RootsTheme", "Twitter Bootstrap", "EShop"},
+		portfolioClients["Tharuni"],
+		[]Feedback{},
 	},
 }
 
@@ -101,6 +111,13 @@ var portfolioCategories = map[string]Category {
 			portfolioItems["IXXOCart"],
 		},
 	},
+	"Websites" : Category {
+		"Websites",
+		"Websites and Templates",
+		[]PortfolioItem {
+			portfolioItems["CheriyalAndPembarthy"],
+		},
+	},
 }
 
 /* Items to be displayed on the home page */
@@ -112,7 +129,5 @@ var homePageItems = []PortfolioItem {
 
 /* Client feedback to be highlighted on the home page */
 var homePageFeedback = []Feedback {
-	portfolioItems["IXXOCart"].Feedback[0],
 	portfolioItems["GeneralKnowitallKnowledge"].Feedback[0],
-	portfolioItems["iEntertain"].Feedback[0],
 }
