f169cfe57c9ec99a6ba65734e956ab6f13bc748a
Sathvik P <psathvik@gmail.com>
2014-07-20 20:13:22 +0530

    cmd tweaks

--- publish.bat
+++ publish.bat
@@ -2,4 +2,4 @@ cd "`dirname $0`"
 git add . -A
 git commit -am "$1"
 git push
-/c/Python/python.exe ../go_appengine/appcfg.py --oauth2 update --no_cookies .
+/c/Python/python.exe /c/go_appengine/appcfg.py --oauth2 update --no_cookies .
